77bd200ea28f5b03239c280a0c3707fe8f4d70c8
StefPan <127962928+StefPan@users.noreply.github.com>
2023-03-30 15:25:57 +0200

    commit class Contorno

---
+++ src/ContornoEntity.java
@@ -0,0 +1,88 @@
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "contorni")
+public class ContornoEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name= "id_contorno")
+	private Integer id;
+	
+	@Column(name ="nome")
+	private String nome;
+	
+	@Column(name= "prezzo")
+	private Double prezzo;
+	
+	
+	@ManyToOne
+	@JoinColumn(name = "id_menù")
+//	private MenuEntity menuEntity;
+	
+
+	public void addContorno(ContornoEntity contornoEntity) {
+		
+	}
+
+	public void removeContorno(ContornoEntity contornoEntity) {
+		;
+	}
+
+	
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getPrezzo() {
+		return prezzo;
+	}
+
+	public void setPrezzo(Double prezzo) {
+		this.prezzo = prezzo;
+	}
+
+	public String getDescrizione() {
+		return descrizione;
+	}
+
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	@Column(name ="descrizione")
+	private String descrizione;
+	
+	@Column(name ="tipo")
+	private String tipo;
+
+}
