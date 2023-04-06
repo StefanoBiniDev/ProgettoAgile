eee4f9961b2d20dd60e043d1fd582784a45894bc
Cosmin Dinca <c.dinca@outlook.it>
2023-04-06 14:42:21 +0200

    Revert "Revert "no message""
    This reverts commit c9cc778ff3dc34a653c144c815a0dfd809549524.

--- src/dbscript/createTable.sql
+++ src/dbscript/createTable.sql
@@ -11,7 +11,17 @@ nome varchar(100) not null,
 prezzo decimal(5,2) NOT NULL, 
 descrizione varchar(100),
 id_menu int not null,
-
 PRIMARY KEY (id_portata),
 foreign key (id_menu) references menu(id_menu)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE primi (
+id_primi INT NOT NULL AUTO_INCREMENT,
+nome VARCHAR (100) NOT NULL,
+prezzo VARCHAR (100) NOT NULL,
+descrizione VARCHAR (100) NOT NULL,
+id_menu INT,
+PRIMARY KEY (id_primi),
+FOREIGN KEY (id_menu) REFERENCES menu(id_menu)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
